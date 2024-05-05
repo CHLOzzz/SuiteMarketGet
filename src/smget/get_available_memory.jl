207ab5d05e3136c7e867ea65290ae285653c36c5
CHLOzzz <chloemccarthyemail@gmail.com>
2024-05-05 06:20:49 -0400

    Removed redundant comments

--- src/smget/get_available_memory.jl
+++ src/smget/get_available_memory.jl
@@ -2,7 +2,7 @@
 
 function get_available_memory()
     # Find available memory dependent on OS being used
-    if Sys.iswindows() # User is on Windows
+    if Sys.iswindows()
         # Get available memory in KB
         mem_info = read(`wmic OS get FreePhysicalMemory /Value`, String)
         mem_kb = parse(Int, split(mem_info, '=')[2])
@@ -11,7 +11,7 @@ function get_available_memory()
         # / 1024^2 for KB to GB, * 0.95 for a buffer
         return mem_kb / 1103764
 
-    elseif Sys.islinux() # User is on Linux
+    elseif Sys.islinux()
         # Get available memory in MB (including SWAP)
         mem_info = read(`free -m`, String)
         mem_lines = split(mem_info, '\n')
@@ -21,7 +21,7 @@ function get_available_memory()
         # / 1024 for MB to GB, * 0.95 for a buffer
         return parse(Int, mem_parts[7]) / 1077
 
-    elseif Sys.isapple() # User is on an Apple product !!!NEEDS TESTING!!!
+    elseif Sys.isapple()
         mem_info = read(`vm_stat`, String)
         mem_lines = split(mem_info, '\n')
         free_line = filter(s -> occursin("Pages free", s), mem_lines)[1]
