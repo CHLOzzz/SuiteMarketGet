4be3324e1e2a28cecd1c47cd861c2cb4bb79c775
CHLOzzz <chloemccarthyemail@gmail.com>
2024-05-09 18:05:34 -0400

    Added check for v4 MAT file

--- src/smget/repurposed_MAT_v4.jl
+++ src/smget/repurposed_MAT_v4.jl
@@ -27,11 +27,45 @@
 # http://www.mathworks.com/help/pdf_doc/matlab/matfile_format.pdf
 
 # Override function for MAT_v4.checkv4
-function repurposed_MAT_v4(data_http::HTTP.Messages.Response, data_buffer::IOBuffer, debug::Bool, keep_data::Bool)
-    println("v4 not supported yet...")
+function repurposed_MAT_v4(data_buffer::IOBuffer)
+    M, O, P, T, mrows, ncols, imagf, namlen = read_v4_header(data_buffer, false)
+    if 0<=M<=4 && O == 0 && 0<=P<=5 && 0<=T<=2 && mrows>=0 && ncols>=0 && 0<=imagf<=1 && namlen>0
+        swap_bytes = false
+        return (true, swap_bytes)
+    else
+        seek(f, 0)   
+        M, O, P, T, mrows, ncols, imagf, namlen = read_v4_header(data_buffer, true)
+        if 0<=M<=4 && O == 0 && 0<=P<=5 && 0<=T<=2 && mrows>=0 && ncols>=0 && 0<=imagf<=1 && namlen>0
+            swap_bytes = true
+            return (true, swap_bytes)
+        end
+    end
+    return (false, false)
+
 end # repurposed_MAT_v4
 
 # Override function for MAT_v4.matopen
 function repurposed_MAT_v4(data_http::HTTP.Messages.Response, data_buffer::IOBuffer, debug::Bool, keep_data::Bool, swap_bytes::Bool)
     println("v4 not supported yet...")
+
 end # repurposed_MAT_v4
+
+read_v4_bswap(f::IO, swap_bytes::Bool, ::Type{T}) where T =
+    swap_bytes ? bswap(read(f, T)) : read(f, T)
+
+# Read data type and number of bytes at the start of a data element
+function read_v4_header(f::IO, swap_bytes::Bool)
+    dtype = read_v4_bswap(f, swap_bytes, Int32)
+
+    M = div(rem(dtype, 10000), 1000)
+    O = div(rem(dtype, 1000), 100)
+    P = div(rem(dtype, 100), 10)
+    T = div(rem(dtype, 10), 1)
+
+    mrows = read_v4_bswap(f, swap_bytes, Int32)
+    ncols = read_v4_bswap(f, swap_bytes, Int32)
+    imagf = read_v4_bswap(f, swap_bytes, Int32)
+    namlen = read_v4_bswap(f, swap_bytes, Int32)
+    
+    M, O, P, T, mrows, ncols, imagf, namlen
+end
